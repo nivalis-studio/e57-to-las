1b6f8f428a97da07976e668647e5c4dcf8ac89b0
Nicolas <pnwa@icloud.com>
2023-08-22 13:59:31 +0200

    chore: replace eventual invalid guid by random uuid

--- src/convert_pointcloud.rs
+++ src/convert_pointcloud.rs
@@ -23,8 +23,8 @@ pub fn convert_pointcloud(
     let mut builder = las::Builder::from((1, 4));
     builder.point_format.has_color = true;
     builder.generating_software = String::from("e57_to_las");
-    builder.guid = Uuid::parse_str(&pointcloud.guid.clone().replace("_", "-"))
-        .context("Invalid pointcloud guid: ")?;
+    builder.guid =
+        Uuid::parse_str(&pointcloud.guid.clone().replace("_", "-")).unwrap_or(Uuid::new_v4());
 
     let header = builder.into_header().context("Error encountered: ")?;
 
