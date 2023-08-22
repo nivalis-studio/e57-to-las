227ff45f8bb6c17ec2a03638414d641fd20e2092
pnodet <paul.nodet@gmail.com>
2023-08-22 20:07:41 +0200

    doc: add rustdoc for the exported functions

--- src/convert_point.rs
+++ src/convert_point.rs
@@ -1,5 +1,23 @@
 use e57::CartesianCoordinate;
 
+/// Converts an e57::Point to an optional las::Point.
+///
+/// This function takes an e57 point, extracts the Cartesian coordinates and optional color and intensity
+/// attributes, and constructs a corresponding las point.
+///
+/// # Parameters
+/// - `point`: The e57 point that needs to be converted.
+///
+/// # Returns
+/// - `Option<las::Point>`: An optional las point. The function returns `None` if the Cartesian coordinates
+///   are not valid. Otherwise, it returns a `Some(las::Point)` containing the converted point.
+///
+/// # Example
+/// ```ignore
+/// use e57_to_las::convert_point;
+/// let e57_point = e57::Point {  };
+/// let las_point = convert_point(e57_point);
+/// ```
 pub fn convert_point(point: e57::Point) -> Option<las::Point> {
     let mut las_point = las::Point::default();
 
--- src/convert_pointcloud.rs
+++ src/convert_pointcloud.rs
@@ -7,6 +7,32 @@ use e57::{E57Reader, PointCloud};
 use las::Write;
 use std::collections::HashMap;
 
+/// Converts a point cloud to a LAS file and returns a map of station points.
+///
+/// This function the points from the point cloud, converts them to LAS points using the `convert_point`
+/// function, and writes them to the LAS file.
+/// Additionally, it calculates the sum of coordinates and returns a hash map containing station
+/// points created with `create_station_point`.
+///
+/// # Parameters
+/// - `index`: The index of the point cloud.
+/// - `pointcloud`: A reference to the point cloud to be converted.
+/// - `input_path`: A reference to the input file path (E57 file).
+/// - `output_path`: A reference to the output dir.
+///
+/// # Returns
+/// - `Result<HashMap<usize, StationPoint>>`: A result containing a hash map that associates the index
+///   with a station point. Returns an error if any part of the conversion fails, including if there
+///   are no points in the point cloud.
+///
+/// # Example
+/// ```ignore
+/// use e57_to_las::convert_pointcloud;
+/// let pointcloud = e57::Pointcloud {  };
+/// let input_path = String::from("path/to/input.e57");
+/// let output_path = String::from("path/to/output");
+/// convert_pointcloud(0, &pointcloud, input_path, output_path);
+/// ```
 pub fn convert_pointcloud(
     index: usize,
     pointcloud: &PointCloud,
