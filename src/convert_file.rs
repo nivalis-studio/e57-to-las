c142becbe0ab2c471cc8b995941396e471bb7044
Nicolas <pnwa@icloud.com>
2023-09-21 19:41:31 +0200

    feat: convert e57 into one las if stations options is false

--- src/convert_file.rs
+++ src/convert_file.rs
@@ -1,11 +1,18 @@
 extern crate rayon;
+use std::sync::Mutex;
+
+use e57::E57Reader;
+use las::Write;
 use rayon::prelude::*;
 
 use anyhow::{Context, Result};
+use uuid::Uuid;
 
+use crate::convert_point;
 use crate::convert_pointcloud::convert_pointcloud;
 
 use crate::stations::save_stations;
+use crate::utils::construct_las_path;
 
 /// Converts a given e57 file into a series of point clouds and station files.
 ///
@@ -45,21 +52,71 @@ pub fn convert_file(
 
     let pointclouds = e57_reader.pointclouds();
 
-    pointclouds
-        .par_iter()
-        .enumerate()
-        .try_for_each(|(index, pointcloud)| -> Result<()> {
-            println!("Saving pointcloud {}...", index);
-
-            convert_pointcloud(index, pointcloud, &input_path, &output_path)
-                .context(format!("Error while converting pointcloud {}", index))?;
-
-            Ok(())
-        })
-        .context("Error during the parallel processing of pointclouds")?;
-
     if save_sations {
+        pointclouds
+            .par_iter()
+            .enumerate()
+            .try_for_each(|(index, pointcloud)| -> Result<()> {
+                println!("Saving pointcloud {}...", index);
+
+                convert_pointcloud(index, pointcloud, &input_path, &output_path)
+                    .context(format!("Error while converting pointcloud {}", index))?;
+
+                Ok(())
+            })
+            .context("Error during the parallel processing of pointclouds")?;
+
         save_stations(output_path, pointclouds)?;
+    } else {
+        // TODO: refactor this to avoid repetition
+        let las_path =
+            construct_las_path(&output_path, 0).context("Unable to create file path: ")?;
+        let mut builder = las::Builder::from((1, 4));
+        builder.point_format.has_color = true;
+        builder.generating_software = String::from("e57_to_las");
+        builder.guid =
+            Uuid::parse_str(&e57_reader.guid().clone().replace("_", "-")).unwrap_or(Uuid::new_v4());
+        let header = builder.into_header().context("Error encountered: ")?;
+
+        let writer =
+            Mutex::new(las::Writer::from_path(&las_path, header).context("Error encountered: ")?);
+
+        pointclouds
+            .par_iter()
+            .enumerate()
+            .try_for_each(|(index, pointcloud)| -> Result<()> {
+                println!("Saving pointclouds {}...", index);
+
+                let mut e57_reader =
+                    E57Reader::from_file(&input_path).context("Failed to open e57 file: ")?;
+
+                let pointcloud_reader = e57_reader
+                    .pointcloud_simple(pointcloud)
+                    .context("Unable to get point cloud iterator: ")?;
+
+                for p in pointcloud_reader {
+                    let point = p.context("Could not read point: ")?;
+
+                    let las_point = match convert_point(point) {
+                        Some(p) => p,
+                        None => continue,
+                    };
+
+                    writer
+                        .lock()
+                        .unwrap()
+                        .write(las_point)
+                        .context("Unable to write: ")?;
+                }
+
+                Ok(())
+            })
+            .context("Error during the parallel processing of pointclouds")?;
+        writer
+            .lock()
+            .unwrap()
+            .close()
+            .context("Failed to close the writer: ")?;
     }
     Ok(())
 }
